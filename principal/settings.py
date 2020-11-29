8861688ffb2f37657c3dd2ca4b47e50f6a03cb64
Rene <rene.exe@gmail.com>
2020-11-29 11:38:08 -0300

    Configurando staticfiles e integração com CDN

--- principal/settings.py
+++ principal/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+    'collectfast',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
